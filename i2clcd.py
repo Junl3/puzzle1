1bfac4c1ac110d3f3de02d37014e5722721252e3
Junle Wang <junle.wang@estudiantat.upc.edu>
2024-10-07 08:28:40 +0200

    Puzzle 1 actualizado

--- i2clcd.py
+++ i2clcd.py
@@ -2,4 +2,5 @@ from i2clcd import i2clcd
 import time
 lcd=i2clcd (1,0x27,20)
 lcd.init()
-lcd.print_long_text("Nuestro grupo de PBE esta compuesto por blanca,ivan,mariona,roger,junle")
+text= input("Escriba lo que quieras mostrar por pantalla: ")
+lcd.print_long_text(text)
